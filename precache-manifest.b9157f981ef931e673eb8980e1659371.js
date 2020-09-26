a799b261a41402efdbde3776f5b2a85d4e7e5961
Usman Zain <usmanzain95@gmail.com>
2020-09-26 14:29:15 +0500

    Updates

---
+++ precache-manifest.b9157f981ef931e673eb8980e1659371.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31fd185207b1a772afea99c107a08a50",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33071ca2820658ddbcc5",
+    "url": "/static/css/main.f041efe3.chunk.css"
+  },
+  {
+    "revision": "3958f5be29d97c7c812e",
+    "url": "/static/js/2.609f4c3e.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.609f4c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33071ca2820658ddbcc5",
+    "url": "/static/js/main.a1f13101.chunk.js"
+  },
+  {
+    "revision": "e4358cefd109110e5fc8",
+    "url": "/static/js/runtime-main.9e3a875a.js"
+  },
+  {
+    "revision": "fb86275a71a01fb935f4ab7cbfba0396",
+    "url": "/static/media/brandmefy.fb86275a.png"
+  },
+  {
+    "revision": "5905037dc5ff21f5040954658126fdad",
+    "url": "/static/media/lafw.5905037d.png"
+  }
+]);
